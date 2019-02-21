ca5706b42f3d68410b07bf95d687169ef5af8016
Rob Ramsey <ramseyrt@gmail.com>
2019-02-21 16:07:52 -0600

    Updating offline code

---
+++ big_data.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -x
+/bin/echo "Creating /share/consolidated_file.log"
+/bin/touch /share/consolidated_file.log
+/bin/echo "Populating BigFiles array with compressed audit log filename objects"
+declare -a BigFiles=($(/bin/find /share/AuditArchive -type f))
+/bin/echo "BigFiles array contains " ${#SmallFiles[@]} " objects"
+/bin/echo "BigFiles array contains the following: " ${BigFiles[@]}
+/bin/echo "Setting counter_a=0"
+counter_a=0
+/bin/echo "Setting counter_b=0"
+counter_b=0
+while [ ${#BigFiles[@]} -gt ${counter_a} ]
+	do
+		/bin/echo "counter_a value is " ${counter_a}
+		/bin/echo "Extracting " ${BigFiles[${counter_a}]} " to /share/big_data"
+		/bin/tar -xzvf ${BigFiles[${counter_a}]} --directory /share/big_data
+		/bin/echo "Populating SmallFiles array with audit log filename objects"
+		declare -a SmallFiles=($(/bin/find /share/big_data -type f))
+		/bin/echo "SmallFiles array contains " ${#SmallFiles[@]} " objects"
+		/bin/echo "SmallFiles array contains the following: " ${SmallFiles[@]}
+
+	while [ ${#SmallFiles[@]} -gt ${counter_b} ]
+		do
+			/bin/echo "counter_a value is " ${counter_a}
+			/bin/echo "Consolidating " ${SmallFiles[${counter_b}]} " into /share/consolidated_file.log"
+			/bin/cat ${SmallFiles[${counter_b}]} >> /share/consolidated_file.log
+			let counter_b+=1
+		done
+
+		/bin/echo "Removing /share/big_data"
+		/bin/rm -rf /share/big_data
+		let counter_a+=1
+done
+
